fb1b21ba6e29009a5451768e26c28a1171ac01c8
mishalverma <vermamishal@gmail.com>
2020-05-22 22:26:15 +0530

    Updates

---
+++ precache-manifest.06c813e4c6ba753fbe2e0b94f1f119e8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8166a8ded42305c458943047c7b5f899",
+    "url": "/RoboMates/index.html"
+  },
+  {
+    "revision": "e316d57d65c04bb2152a",
+    "url": "/RoboMates/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "e601c0109feba122b0e3",
+    "url": "/RoboMates/static/css/main.c9f1d309.chunk.css"
+  },
+  {
+    "revision": "e316d57d65c04bb2152a",
+    "url": "/RoboMates/static/js/2.8f99e9e4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RoboMates/static/js/2.8f99e9e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e601c0109feba122b0e3",
+    "url": "/RoboMates/static/js/main.541dc803.chunk.js"
+  },
+  {
+    "revision": "d44b5d939a19803a00cd",
+    "url": "/RoboMates/static/js/runtime-main.52419b05.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/RoboMates/static/media/SEGA.12a3db8b.woff"
+  }
+]);
